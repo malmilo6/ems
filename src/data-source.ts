706cdf8adadd89e38b518373213656d8e1c8de90
malmilo6 <maxim.cernetchi@isa.utm.md>
2025-08-22 01:34:52 +0300

    feat: containerization + postgre

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || 'db',
     port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    username: process.env.DB_USER || 'erp_usr',
+    password: process.env.DB_PASS || 'secret',
+    database: process.env.DB_NAME || 'erp',
     entities: [],
     migrations: ['src/migrations/*.ts'],
     synchronize: false   // off in prod, use migrations
